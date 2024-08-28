3d9d73d99728ae932b2ead495f081d3490a0a4ca
natesworksdev <gh@natesworks.com>
2024-08-28 18:32:32 +0200

    Please work

--- app/submit/page.tsx
+++ app/submit/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { DownloadIcon, MailIcon, MessageCircleIcon, Search as SearchIcon } from "lucide-react"
+import { MailIcon, MessageCircleIcon } from "lucide-react"
 import Link from "next/link"
 
 export default function ModSubmissionForm() {
@@ -44,7 +44,7 @@ export default function ModSubmissionForm() {
               <textarea 
                 id="modDescription" 
                 name="modDescription" 
-                rows=4
+                rows={4}
                 className="mt-1 p-2 block w-full border border-gray-300 rounded-md dark:bg-gray-700 dark:text-gray-300" 
                 required 
               />
