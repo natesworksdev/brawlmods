617b959d40b0367d34995fbeb9e1517be678e331
Natesworks <gh@natesworks.com>
2024-08-31 14:32:51 +0200

    Fix gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,14 @@ import Footer from './Footer';
 import { ThemeProvider } from 'next-themes';
 import { Metadata } from 'next';
 import './globals.css';
-import Head from 'next/head';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Brawl Stars Mods',
   description: 'Explore and download mods for Brawl Stars',
-}
+};
 
 export default function RootLayout({
   children,
@@ -20,19 +20,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6K4D86LZCP"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-6K4D86LZCP');
-            `,
-          }}
-        />
-      </Head>
       <body className={`${inter.className} flex flex-col min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Header />
@@ -40,6 +27,22 @@ export default function RootLayout({
             {children}
           </main>
           <Footer />
+          <Script
+            strategy="afterInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=G-6K4D86LZCP"
+          />
+          <Script
+            id="google-analytics"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-6K4D86LZCP');
+              `,
+            }}
+          />
         </ThemeProvider>
       </body>
     </html>
