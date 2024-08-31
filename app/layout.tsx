2cabbcb8a7c10dea3cacb252a1033a4e019b7129
Natesworks <gh@natesworks.com>
2024-08-31 14:29:06 +0200

    Add gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Footer from './Footer';
 import { ThemeProvider } from 'next-themes';
 import { Metadata } from 'next';
 import './globals.css';
-import next from 'next';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,6 +20,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-6K4D86LZCP"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-6K4D86LZCP');
+            `,
+          }}
+        />
+      </Head>
       <body className={`${inter.className} flex flex-col min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 dark:from-gray-900 dark:to-gray-800`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Header />
